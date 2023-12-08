9a17ed7e3891d1cb7203b7661e21bd70a79cc076
yihong <yihong6372@gmail.com>
2023-12-08 21:33:30 +0800

    添加 ant design vue

--- web/src/components/HelloWorld.vue
+++ web/src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <a-button type="primary" danger>Primary</a-button>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the

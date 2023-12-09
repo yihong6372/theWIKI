2d0eb869b74cd600ada9b10bbc5860a9b859e2d5
yihong <yihong6372@gmail.com>
2023-12-09 10:24:07 +0800

    页面改名，小写首字母开头

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '../views/homeView.vue'
+import AboutView from '../views/aboutView.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -9,11 +10,12 @@ const routes: Array<RouteRecordRaw> = [
   },
   {
     path: '/about',
-    name: 'about',
+    name: 'About',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    // component: () => import(/* webpackChunkName: "about" */ '../views/aboutView.vue')
+    component: AboutView
   }
 ]
 

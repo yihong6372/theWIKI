0190b27cfa4c47b85ca2b8f4242e70670b228acd
yihong <yihong6372@gmail.com>
2023-12-09 09:40:12 +0800

    添加拦截器interceptor

---
+++ src/main/java/net/geekh/wiki/config/SpringMvcConfig.java
@@ -0,0 +1,26 @@
+package net.geekh.wiki.config;
+
+import net.geekh.wiki.interceptor.LogInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @Author YIHONG
+ * @Description
+ * @Date 2023/12/9 09:24
+ */
+@Configuration
+public class SpringMvcConfig implements WebMvcConfigurer {
+
+
+    @Autowired
+    private LogInterceptor logInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(logInterceptor)
+                .addPathPatterns("/**");
+    }
+}
---
+++ src/main/java/net/geekh/wiki/interceptor/LogInterceptor.java
@@ -0,0 +1,38 @@
+package net.geekh.wiki.interceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @Author YIHONG
+ * @Description
+ * @Date 2023/12/9 09:12
+ */
+@Component
+public class LogInterceptor implements HandlerInterceptor {
+
+    private static final Logger Log = LoggerFactory.getLogger(LogInterceptor.class);
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        Log.info("-------------- LogInterceptor开始 -------------------");
+        Log.info("请求地址：{} {} ", request.getRequestURI(), request.getMethod());
+        Log.info(("远程地址: {} "), request.getRemoteAddr());
+
+        long startTime = System.currentTimeMillis();
+        request.setAttribute("requestStartTime", startTime);
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        long startTime = (Long) request.getAttribute("requestStartTIme");
+        Log.info("--------------LogInterceptor 结束耗时 {} ms -----------------",System.currentTimeMillis() - startTime);
+    }
+}
